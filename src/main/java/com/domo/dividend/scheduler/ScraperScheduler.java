944a26c1629eb8e451f07a78e2eb4afce874997e
kdomo <dongho5088@naver.com>
2022-12-20 14:09:31 +0900

    [#18] feat : redis를 캐시 저장소로 사용

--- src/main/java/com/domo/dividend/scheduler/ScraperScheduler.java
+++ src/main/java/com/domo/dividend/scheduler/ScraperScheduler.java
@@ -2,6 +2,7 @@ package com.domo.dividend.scheduler;
 
 import com.domo.dividend.model.Company;
 import com.domo.dividend.model.ScrapedResult;
+import com.domo.dividend.model.constants.CacheKey;
 import com.domo.dividend.persist.CompanyRepository;
 import com.domo.dividend.persist.DividendRepository;
 import com.domo.dividend.persist.entity.CompanyEntity;
@@ -10,11 +11,14 @@ import com.domo.dividend.scraper.Scraper;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@EnableCaching
 @AllArgsConstructor
 public class ScraperScheduler {
 
@@ -22,6 +26,7 @@ public class ScraperScheduler {
     private final DividendRepository dividendRepository;
     private final Scraper yahooFinanceScraper;
 
+    @CacheEvict(value = CacheKey.KEY_FINANCE, allEntries = true)
     @Scheduled(cron = "${scheduler.scrap.yahoo}")
     public void yahooFinanceScheduling() {
         List<CompanyEntity> companies = this.companyRepository.findAll();
